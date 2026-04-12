1631ee5181dd45b2250768a5a305f7cb6f7d1da5
claytonsilva <clayton@cavaleiro.dev>
2026-04-12 00:04:08 -0300

    chore(cluster): add state

--- terraform/bootstrap/cluster/provider.tf
+++ terraform/bootstrap/cluster/provider.tf
@@ -5,8 +5,17 @@ terraform {
       version = "0.10.1"
     }
   }
+  required_version = ">= 1.5.7"
 }
 
-provider "talos" {
-  # Configuration options
+terraform {
+  backend "s3" {
+    bucket         = "catdevsecops-terraform-state"
+    key            = "bootstrap/cluster.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
 }
+
+provider "talos" {}
