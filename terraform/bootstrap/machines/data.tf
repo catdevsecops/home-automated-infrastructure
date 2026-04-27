2d7e9a256339a2390a08234196d84364f6024993
claytonsilva <clayton@cavaleiro.dev>
2026-04-26 23:26:22 -0300

    feat(local-storage): added dind volume talos

--- terraform/bootstrap/machines/data.tf
+++ terraform/bootstrap/machines/data.tf
@@ -24,7 +24,11 @@ data "talos_machine_configuration" "controlplane" {
   machine_secrets  = talos_machine_secrets.this.machine_secrets
   talos_version    = var.talos_version
 
-  config_patches = [local.general_patch, local.machinename_controlplane_patch[each.key], local.machinename_controlplane_kubelet_patch[each.key]]
+  config_patches = [
+    local.general_patch,
+    local.machinename_controlplane_patch[each.key],
+    local.machinename_controlplane_kubelet_patch[each.key]
+  ]
 }
 
 # ---------------------------------------------------------------------------
@@ -39,6 +43,10 @@ data "talos_machine_configuration" "worker" {
   machine_secrets  = talos_machine_secrets.this.machine_secrets
   talos_version    = var.talos_version
 
-  config_patches = [local.machinename_workers_patch[each.key], local.general_patch, local.machinename_workers_kubelet_patch[each.key]]
+  config_patches = [
+    local.machinename_workers_patch[each.key],
+    local.general_patch,
+    local.machinename_workers_kubelet_patch[each.key]
+  ]
 }
 
