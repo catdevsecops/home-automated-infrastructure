cf63ea4e520a65237929b6b587a4126a40c9b31a
claytonsilva <clayton@cavaleiro.dev>
2026-05-08 16:42:25 -0300

    feat(aws): added fixed cost tags

--- terraform/us-east-2/ssm/cloudflared/provider.tf
+++ terraform/us-east-2/ssm/cloudflared/provider.tf
@@ -22,6 +22,13 @@ terraform {
 
 provider "aws" {
   region = "us-east-2"
+  default_tags {
+    tags = {
+      Environment = "Production"
+      ManagedBy   = "terraform"
+      Project     = "home-automated-infrastructure"
+    }
+  }
 }
 
 provider "kubernetes" {}
